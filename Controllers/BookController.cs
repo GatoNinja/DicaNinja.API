f195033f9273c49351f8b66ccca7cfb507e2d833
Ygor Lazaro <ygor@ygorlazaro.com>
2022-05-24 17:30:26 -0300

    First commit

---
+++ Controllers/BookController.cs
@@ -0,0 +1,35 @@
+﻿using Google.Apis.Books.v1;
+using Google.Apis.Services;
+
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookSearch.API.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class BookController : ControllerBase
+{
+    private const string apiKey = "AIzaSyBKobq7aC-ajuflWLdnrjGlFnz-Eem3Fhw";
+
+    public BookController()
+    {
+        var service = new BooksService(new BaseClientService.Initializer()
+        {
+            ApiKey = apiKey,
+            ApplicationName = "Fenicia",
+        });
+
+        Service = service;
+    }
+
+    private BooksService Service { get; }
+
+    [HttpGet]
+    public async Task<IActionResult> GetAsync([FromQuery] string query)
+    {
+        var request = Service.Volumes.List(query);
+        var response = await request.ExecuteAsync();
+
+        return Ok(response.Items.Select(item => item.VolumeInfo));
+    }
+}
